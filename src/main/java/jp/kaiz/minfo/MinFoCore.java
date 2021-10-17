99d929f728372feec9a647633d601d1470d8b140
Kaiz <kaiz@kaiz.jp>
2021-10-17 16:55:41 +0900

    1.1.0

--- src/main/java/jp/kaiz/minfo/MinFoCore.java
+++ src/main/java/jp/kaiz/minfo/MinFoCore.java
@@ -6,7 +6,7 @@ import cpw.mods.fml.common.Mod;
 public class MinFoCore {
     public static final String MODID = "MinFo";
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     @Mod.Instance(MODID)
     public static MinFoCore INSTANCE;
