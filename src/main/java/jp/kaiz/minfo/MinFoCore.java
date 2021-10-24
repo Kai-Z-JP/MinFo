2729ca2564f52b4b2a8da35d3cf3180181e5ef25
Kaiz <kaiz@kaiz.jp>
2021-10-24 20:21:31 +0900

    1.2.0

--- src/main/java/jp/kaiz/minfo/MinFoCore.java
+++ src/main/java/jp/kaiz/minfo/MinFoCore.java
@@ -11,7 +11,7 @@ import net.minecraftforge.event.world.WorldEvent;
 public class MinFoCore {
     public static final String MODID = "MinFo";
 
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
 
     @Mod.Instance(MODID)
     public static MinFoCore INSTANCE;
