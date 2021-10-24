73db32b9c5ff889ac9a8d65424daba59bdb16fe3
Kaiz <kaiz@kaiz.jp>
2021-10-24 20:21:19 +0900

    フォントの事前読み込みに対応

--- src/main/java/jp/kaiz/minfo/MinFoCore.java
+++ src/main/java/jp/kaiz/minfo/MinFoCore.java
@@ -1,6 +1,11 @@
 package jp.kaiz.minfo;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import jp.kaiz.minfo.api.MinFoCustomFontRenderer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.WorldEvent;
 
 @Mod(modid = MinFoCore.MODID, version = MinFoCore.VERSION, name = MinFoCore.MODID)
 public class MinFoCore {
@@ -10,4 +15,17 @@ public class MinFoCore {
 
     @Mod.Instance(MODID)
     public static MinFoCore INSTANCE;
+
+
+    @Mod.EventHandler
+    public void init(FMLPreInitializationEvent FMLInitializationEvent) {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onLoadWorld(WorldEvent.Load e) {
+        if (e.world.isRemote) {
+            MinFoCustomFontRenderer.loadedFontList.forEach(MinFoCustomFontRenderer::loadJapaneseCharSets);
+        }
+    }
 }
--- src/main/java/jp/kaiz/minfo/api/MinFoCustomFontRenderer.java
+++ src/main/java/jp/kaiz/minfo/api/MinFoCustomFontRenderer.java
@@ -11,9 +11,13 @@ import org.newdawn.slick.font.effects.ColorEffect;
 import java.awt.*;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class MinFoCustomFontRenderer extends FontRenderer {
+    public static final List<MinFoCustomFontRenderer> loadedFontList = new CopyOnWriteArrayList<>();
     private UnicodeFont uFont;
+    private boolean glyphsLoaded = false;
 
     //必ずTTF
     //OTFはTYPE2だから無理
@@ -23,9 +27,9 @@ public class MinFoCustomFontRenderer extends FontRenderer {
         try (InputStream fontStream = Minecraft.getMinecraft().getResourceManager().getResource(location).getInputStream()) {
             if (fontStream != null) {
                 this.uFont = new UnicodeFont(Font.createFont(Font.TRUETYPE_FONT, fontStream).deriveFont((float) fontSize));
-                this.loadJapaneseCharSets();
+                loadedFontList.add(this);
             }
-        } catch (FontFormatException | IOException | SlickException exception) {
+        } catch (FontFormatException | IOException exception) {
             throw new RuntimeException(exception);
         }
     }
@@ -34,20 +38,27 @@ public class MinFoCustomFontRenderer extends FontRenderer {
     public MinFoCustomFontRenderer(String name, int fontSize) {
         super(Minecraft.getMinecraft().gameSettings, null, null, false);
         this.uFont = new UnicodeFont(new Font(name, Font.PLAIN, fontSize));
-        try {
-            this.loadJapaneseCharSets();
-        } catch (SlickException e) {
-            e.printStackTrace();
-        }
+        loadedFontList.add(this);
     }
 
-    private void loadJapaneseCharSets() throws SlickException {
-        //http://welovy.hatenablog.com/?page=1358762469
-        this.uFont.addAsciiGlyphs();
-        this.uFont.addGlyphs(0x3000, 0x30ff); // Hiragana + katakanab + fullwidth punctuations
-        this.uFont.addGlyphs(0x4e00, 0x9fc0); // Kanji
-        this.uFont.getEffects().add(new ColorEffect(java.awt.Color.WHITE));
-        this.uFont.loadGlyphs();
+    public UnicodeFont getUnicodeFont() {
+        return this.uFont;
+    }
+
+    public void loadJapaneseCharSets() {
+        if (!this.glyphsLoaded) {
+            try {
+                //http://welovy.hatenablog.com/?page=1358762469
+                this.uFont.addAsciiGlyphs();
+                this.uFont.addGlyphs(0x3000, 0x30ff); // Hiragana + katakana + fullwidth punctuations
+                this.uFont.addGlyphs(0x4e00, 0x9fc0); // Kanji
+                this.uFont.getEffects().add(new ColorEffect(java.awt.Color.WHITE));
+                this.uFont.loadGlyphs();
+                this.glyphsLoaded = true;
+            } catch (SlickException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -55,6 +66,7 @@ public class MinFoCustomFontRenderer extends FontRenderer {
         if (string == null) {
             return 0;
         } else {
+            this.loadJapaneseCharSets();
             GL11.glPushMatrix();
             GL11.glEnable(GL11.GL_BLEND);
             GL11.glDisable(GL11.GL_LIGHTING);
@@ -80,6 +92,7 @@ public class MinFoCustomFontRenderer extends FontRenderer {
 
     @Override
     public int getStringWidth(String p_78256_1_) {
+        this.loadJapaneseCharSets();
         return this.uFont.getWidth(p_78256_1_) / 2;
     }
 
