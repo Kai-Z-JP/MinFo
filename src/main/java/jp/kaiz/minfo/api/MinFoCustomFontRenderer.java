d6a38118d68b3013d8cfa25ddf2dd4d4661f18f9
Kaiz <kaiz@kaiz.jp>
2026-04-27 00:53:42 +0900

    描画呼び出しの実行をメインスレッドに強制

--- src/main/java/jp/kaiz/minfo/api/MinFoCustomFontRenderer.java
+++ src/main/java/jp/kaiz/minfo/api/MinFoCustomFontRenderer.java
@@ -1,7 +1,6 @@
 package jp.kaiz.minfo.api;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 import org.newdawn.slick.SlickException;
@@ -12,40 +11,119 @@ import java.awt.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
-public class MinFoCustomFontRenderer extends FontRenderer {
+public class MinFoCustomFontRenderer {
     public static final List<MinFoCustomFontRenderer> loadedFontList = new CopyOnWriteArrayList<>();
+    public int FONT_HEIGHT;
+
+    private final ResourceLocation location;
+    private final String name;
+    private final int fontSize;
     private UnicodeFont uFont;
-    private boolean glyphsLoaded = false;
+    private volatile boolean glyphsLoaded = false;
 
     //必ずTTF
     //OTFはTYPE2だから無理
     public MinFoCustomFontRenderer(ResourceLocation location, int fontSize) {
-        super(Minecraft.getMinecraft().gameSettings, null, null, false);
-
-        try (InputStream fontStream = Minecraft.getMinecraft().getResourceManager().getResource(location).getInputStream()) {
-            if (fontStream != null) {
-                this.uFont = new UnicodeFont(Font.createFont(Font.TRUETYPE_FONT, fontStream).deriveFont((float) fontSize));
-                loadedFontList.add(this);
-            }
-        } catch (FontFormatException | IOException exception) {
-            throw new RuntimeException(exception);
-        }
+        this.location = location;
+        this.name = null;
+        this.fontSize = fontSize;
+        this.FONT_HEIGHT = fontSize;
+        loadedFontList.add(this);
     }
 
 
     public MinFoCustomFontRenderer(String name, int fontSize) {
-        super(Minecraft.getMinecraft().gameSettings, null, null, false);
-        this.uFont = new UnicodeFont(new Font(name, Font.PLAIN, fontSize));
+        this.location = null;
+        this.name = name;
+        this.fontSize = fontSize;
+        this.FONT_HEIGHT = fontSize;
         loadedFontList.add(this);
     }
 
+    public static MinFoCustomFontRenderer create(final ResourceLocation location, final int fontSize) {
+        return new MinFoCustomFontRenderer(location, fontSize);
+    }
+
+    public static MinFoCustomFontRenderer create(final String name, final int fontSize) {
+        return new MinFoCustomFontRenderer(name, fontSize);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T runOnMinecraftThread(Callable<T> callable) {
+        Minecraft minecraft = Minecraft.getMinecraft();
+        if (minecraft.func_152345_ab()) {
+            try {
+                return callable.call();
+            } catch (Exception exception) {
+                throw asRuntimeException(exception);
+            }
+        }
+
+        Future<T> future = minecraft.func_152343_a(callable);
+        try {
+            return future.get();
+        } catch (InterruptedException exception) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(exception);
+        } catch (ExecutionException exception) {
+            throw asRuntimeException(exception.getCause());
+        }
+    }
+
+    private static RuntimeException asRuntimeException(Throwable throwable) {
+        if (throwable instanceof RuntimeException) {
+            return (RuntimeException) throwable;
+        }
+        if (throwable instanceof Error) {
+            throw (Error) throwable;
+        }
+        return new RuntimeException(throwable);
+    }
+
     public UnicodeFont getUnicodeFont() {
+        this.initFont();
         return this.uFont;
     }
 
+    private void initFont() {
+        runOnMinecraftThread((Callable<Void>) () -> {
+            initFontOnMinecraftThread();
+            return null;
+        });
+    }
+
+    private synchronized void initFontOnMinecraftThread() {
+        if (this.uFont != null) {
+            return;
+        }
+
+        if (this.location != null) {
+            try (InputStream fontStream = Minecraft.getMinecraft().getResourceManager().getResource(this.location).getInputStream()) {
+                if (fontStream != null) {
+                    this.uFont = new UnicodeFont(Font.createFont(Font.TRUETYPE_FONT, fontStream).deriveFont((float) this.fontSize));
+                }
+            } catch (FontFormatException | IOException exception) {
+                throw new RuntimeException(exception);
+            }
+        } else {
+            this.uFont = new UnicodeFont(new Font(this.name, Font.PLAIN, this.fontSize));
+        }
+    }
+
     public void loadCJKCharSets() {
+        runOnMinecraftThread((Callable<Void>) () -> {
+            loadCJKCharSetsOnMinecraftThread();
+            return null;
+        });
+    }
+
+    private synchronized void loadCJKCharSetsOnMinecraftThread() {
+        this.initFontOnMinecraftThread();
         if (!this.glyphsLoaded) {
             try {
                 //http://welovy.hatenablog.com/?page=1358762469
@@ -63,7 +141,6 @@ public class MinFoCustomFontRenderer extends FontRenderer {
         }
     }
 
-    @Override
     public int drawString(String string, int x, int y, int color) {
         if (string == null) {
             return 0;
@@ -82,6 +159,18 @@ public class MinFoCustomFontRenderer extends FontRenderer {
         }
     }
 
+    public int drawString(String string, int x, int y, int color, boolean dropShadow) {
+        return drawString(string, x, y, color);
+    }
+
+    public int func_78276_b(String string, int x, int y, int color) {
+        return drawString(string, x, y, color);
+    }
+
+    public int func_78275_b(String string, int x, int y, int color, boolean dropShadow) {
+        return drawString(string, x, y, color, dropShadow);
+    }
+
     public void drawText(String string, int color) {
         if (string != null) {
             this.loadCJKCharSets();
@@ -98,27 +187,32 @@ public class MinFoCustomFontRenderer extends FontRenderer {
         }
     }
 
-    @Override
     public int drawStringWithShadow(String string, int x, int y, int color) {
         return drawString(string, x, y, color);
     }
 
-    @Override
+    public int func_78261_a(String string, int x, int y, int color) {
+        return drawStringWithShadow(string, x, y, color);
+    }
+
     public int getCharWidth(char c) {
         return getStringWidth(Character.toString(c));
     }
 
-    @Override
+    public int func_78263_a(char c) {
+        return getCharWidth(c);
+    }
+
     public int getStringWidth(String p_78256_1_) {
         this.loadCJKCharSets();
         return this.uFont.getWidth(p_78256_1_);
     }
 
-    public int getTextWidth(String text) {
-        return this.getStringWidth(text);
+    public int func_78256_a(String text) {
+        return getStringWidth(text);
     }
 
-    @Override
-    protected void bindTexture(ResourceLocation location) {
+    public int getTextWidth(String text) {
+        return this.getStringWidth(text);
     }
 }
